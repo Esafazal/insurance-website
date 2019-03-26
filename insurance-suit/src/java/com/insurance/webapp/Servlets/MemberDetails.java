01af06fcc6456192db45670666bbdb94b821aeb7
Pathum98 <anthony.pathum934@gmail.com>
2019-03-27 00:25:39 +0530

    memberDtails servlet

---
+++ insurance-suit/src/java/com/insurance/webapp/Servlets/MemberDetails.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.insurance.webapp.Servlets;
+
+import com.insurance.webapp.Dao.QueryDao;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author DELL
+ */
+@WebServlet(name = "MemberDetails", urlPatterns = {"/MemberDetails"})
+public class MemberDetails extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        QueryDao queryDao = new QueryDao();
+        List memberList = queryDao.getMemberDetails("1");
+        
+        request.setAttribute("memberList",memberList);
+        getServletContext().getRequestDispatcher("/userJsp/userProfile.jsp").forward(request, response);
+//        response.sendRedirect("/userJsp/userProfile.jsp");
+                
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
