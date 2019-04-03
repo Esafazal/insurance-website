c55c4499abe7875b2a968b3839a644212f0d5c6b
SachinBhanuka <keragalasachin@gmail.com>
2019-04-03 23:15:40 +0530

    added payment status pending approvals added queries in querydao

--- insurance-suit/src/java/com/insurance/webapp/Dao/QueryDao.java
+++ insurance-suit/src/java/com/insurance/webapp/Dao/QueryDao.java
@@ -10,6 +10,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -181,16 +183,16 @@ public class QueryDao {
         return match;
     }
 
-    public Member getMemberDetails(String memberId) {
+    public Member getMemberDetails(int memberID) {
 
         Member member = new Member();
         try {
             Connection connection = DBConnection.getConnection();
-            String query = "SELECT * FROM `Member` WHERE username = ? ";
+            String query = "SELECT * FROM `Member` WHERE member_id = ? ";
 
             PreparedStatement preparedStatement = connection.prepareStatement(query);
 
-            preparedStatement.setString(1, memberId);
+            preparedStatement.setInt(1, memberID);
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -321,4 +323,223 @@ public class QueryDao {
         return vehicleNumber;
     }
 
+    public int getPendingApprovals() {
+        int count = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT COUNT(*) AS rowcount FROM Member WHERE status='pending';";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                count = resultSet.getInt("rowcount");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
+    }
+
+    public int getPendingClaims() {
+        int count = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT COUNT(*) AS rowcount FROM Claim WHERE status='pending';";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                count = resultSet.getInt("rowcount");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
+    }
+
+    public int getPendingPayments() {
+        int count = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT COUNT(*) AS rowcount FROM payment WHERE status='pending';";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                count = resultSet.getInt("rowcount");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
+    }
+
+    public List<Member> getNewRegistrations() {
+
+        List<Member> registers = new ArrayList<>();
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT * FROM Member WHERE status='pending'";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Member member = new Member();
+                member.setMember_id(resultSet.getInt("member_id"));
+                member.setFirst_name(resultSet.getString("first_name"));
+                member.setLast_name(resultSet.getString("last_name"));
+                member.setAddress(resultSet.getString("address"));
+                member.setNic(resultSet.getString("nic"));
+                member.setDate_of_registration(resultSet.getDate("date_of_registration"));
+                member.setEmail(resultSet.getString("email"));
+                member.setPhone_no(resultSet.getString("phone_no"));
+                registers.add(member);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return registers;
+    }
+
+    public int ifAcceptMember(String memberID) {
+        int rowsAffected = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "UPDATE Member SET status='accepted' WHERE member_id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, memberID);
+            if (preparedStatement.execute()) {
+                rowsAffected++;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rowsAffected;
+    }
+
+    public int ifRejectMember(String memberID) {
+        int rowsAffected = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "UPDATE Member SET status='rejected' WHERE member_id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, memberID);
+            if (preparedStatement.execute()) {
+                rowsAffected++;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rowsAffected;
+    }
+
+    public int addMemberPayment(String memberID, int amount) {
+        int rowsAffected = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "INSERT INTO payment(member_id, amount) VALUES(?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, memberID);
+            preparedStatement.setInt(2, amount);
+            if (preparedStatement.execute()) {
+                rowsAffected++;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rowsAffected;
+    }
+
+    public int getMembershipFee(String vehicleType) {
+        int amount = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT charge_amount FROM membership_fee WHERE type=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, vehicleType);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                amount = resultSet.getInt("charge_amount");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return amount;
+    }
+
+    public String getVehicleType(String memberID) {
+        String type = null;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT vehicle_type FROM Vehicle WHERE member_id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, memberID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                type = resultSet.getString("vehicle_type");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return type;
+    }
+
+    public List<Member> getNotPayedMembers() {
+        List<Member> payments = new ArrayList<>();
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT m.first_name, m.last_name, m.email, m.date_of_registration, m.phone_no, p.amount, p.member_id \n"
+                    + "FROM Member m, payment p \n"
+                    + "WHERE p.member_id = m.member_id AND m.status = 'accepted'";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Member member = new Member();
+                member.setMember_id(resultSet.getInt("member_id"));
+                member.setFirst_name(resultSet.getString("first_name"));
+                member.setLast_name(resultSet.getString("last_name"));
+                member.setDate_of_registration(resultSet.getDate("date_of_registration"));
+                member.setEmail(resultSet.getString("email"));
+                member.setPhone_no(resultSet.getString("phone_no"));
+                member.setAmount(resultSet.getInt("amount"));
+                payments.add(member);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return payments;
+    }
+
+    public int getMemberPayableAmount(String memberID) {
+        int amount = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "SELECT amount FROM payment WHERE member_id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, memberID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                amount = resultSet.getInt("amount");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return amount;
+    }
+
+    public int ifSuspendMember(String memberID) {
+        int rowsAffected = 0;
+        try {
+            Connection connection = DBConnection.getConnection();
+            String query = "UPDATE Member SET status='suspended' WHERE member_id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, memberID);
+            if (preparedStatement.execute()) {
+                rowsAffected++;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(QueryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rowsAffected;
+    }
 }
